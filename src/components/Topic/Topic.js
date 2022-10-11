7d1a02f54d52cda8232c26b6610a628399008dc9
46ra20 <rakib2046.md@gmail.com>
2022-10-11 12:52:33 -0400

    finelly done

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -10,7 +10,7 @@ const Topic = () => {
         <div className='container mt-5'>
             <div className='mx-auto mx-md-2 border-bottom d-flex align-items-center bg-light rounded' style={{marginTop:'75px'}}>
                 <img src={Img} alt='education is power' style={{height:'140px',width:'200px'}} className='rounded-1'></img>
-                <div className='ps-3'>
+                <div className='ps-3 text-center flex-grow-1'>
                     <h3>Practice make man perfect.</h3>
                     <p>Sharp your Knowledge with smart way!!!</p>
                 </div>
