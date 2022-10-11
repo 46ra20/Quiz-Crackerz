fbef9b20370f4faf90d9964b80bcbd3f923735cd
46ra20 <rakib2046.md@gmail.com>
2022-10-11 05:16:17 -0400

    error components compleate

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className='w-50 mx-auto my-5 border rounded p-4 text-center'>
+            <h3>Sorry we can't find any page with this link.</h3>
+            <h4>Please Make Sure</h4>
+            <h4>Or go <Link to={'/topic'}>Home</Link></h4>
         </div>
     );
 };
